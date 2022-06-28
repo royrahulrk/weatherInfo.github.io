37751e04d5f5fabe905caf5ced6336487c6fc166
Rahul Kumar Meenaa <99737908+royrahulrk@users.noreply.github.com>
2022-06-28 13:45:54 +0530

    myweatherinfo

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+// import Resturant from './component/Bas/Resturant';
+// import UseState from './component/hooks/useState';
+import Temp from './component/weather/temp';
+const App = () => {
+  return(
+    <div>
+      <Temp />
+    </div>
+  )
+  
+}
+
+export default App
